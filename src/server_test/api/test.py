8aca880ffc890eb0b669eb834bb7c330c65ed0df
DaeYoung96 <zczczcc123@naver.com>
2020-05-06 12:49:27 +0900

    json server app test

--- src/server_test/api/test.py
+++ src/server_test/api/test.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
-@app.route('/')
-def test():
-   return render_template('sample.html')
+@app.route('/test')
+#def test():
+#  return render_template('sample.html')
+def json_test():
+    user_jon = request.get_json()
+    print(user_jon)
+    return jsonify(user_json)
+    
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
