cef6cdd633656477876019cf28e6f877865e6f60
sungpyo9053 <44603374+sungpyo9053@users.noreply.github.com>
2020-05-15 17:13:16 +0900

    Update reverse_alarm.py

--- src/reverse_alarm.py
+++ src/reverse_alarm.py
@@ -35,21 +35,21 @@ def prettifyTime(time, x):
     min = time % 60
     return "{:02d}:{:02d}{}".format(int(hour), int(min), 'am' if x == 1 else 'pm')
 
-if __name__ == '__main__':
-    menu = 1
-    while True:
-        
-        if menu == 0:
-            hour, min = map(int, input("When would you like to wake up? \n").split(':'))
-            results = calculateWaketimes(hour, min)
-            print (results)
-        elif menu == 1:
-            hour, min = map(int, input("When would you like to wake up? \n").split(':'))
-            results = calculateWaketimes(hour, min)
-            print (results)
-        elif menu == 2:
-            print ("Quitting.")
-            break #quit
-        else:
+#if __name__ == '__main__':
+#    menu = 1
+#    while True:
+#        
+#        if menu == 0:
+#            hour, min = map(int, input("When would you like to wake up? \n").split(':'))
+#            results = calculateWaketimes(hour, min)
+#            print (results)
+#        elif menu == 1:
+#            hour, min = map(int, input("When would you like to wake up? \n").split(':'))
+#            results = calculateWaketimes(hour, min)
+#            print (results)
+#        elif menu == 2:
+#           print ("Quitting.")
+#           break #quit
+#        else:
             print ("Invalid selection. Please select one of the menu items by entering its number.")
             continue #skip to next iteration
