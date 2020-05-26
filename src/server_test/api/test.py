e94cd510d8ae4d915f25b86368856629dd235df3
Ubuntu <ubuntu@ip-172-31-24-125.ap-northeast-2.compute.internal>
2020-05-26 18:27:37 +0000

    android url connection not main thead / AsyncTask use

--- src/server_test/api/test.py
+++ src/server_test/api/test.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template, request, jsonify
 import call_moving_sec
 app = Flask(__name__)
 
-@app.route('/test', methods = ['POST'])
-# def test():
-#    return render_template('sample.html')
+arr = []
+
+@app.route('/test', methods = ['GET'])
 def req_test():
     tmp = call_moving_sec.call_req()
     json_obj = call_moving_sec.call_res(tmp)
@@ -12,6 +12,21 @@ def req_test():
     # user_json = request.get_json()
     print(json_obj)
     return json_obj
+
+@app.route('/in', methods = ['POST'])
+def in_test():
+    #id = request.form['id']
+    #name = request.form['name']
+    #form = request.form['form']
+
+    test = request
+    arr.append(test)
+    #arr.append(id)
+    #arr.append(name)
+    #arr.append(form)
+    print(arr)
+    return jsonify({'message' : 'sucess'}) 
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
