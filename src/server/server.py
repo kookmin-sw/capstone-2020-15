9cc9a15cd9d30add336b5903593f4b2055f2c1e5
unknown <zczczcc123@naver.com>
2020-06-09 19:51:27 +0900

    reverse_alarm.py setting

--- src/server/server.py
+++ src/server/server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, jsonify
-import map_api, extractor
+import map_api, extractor, reverse_alarm
 app = Flask(__name__)
 
 arr = []
@@ -12,6 +12,17 @@ def req_test():
     #print(tmp)
     return jsonify(tmp)
 
+@app.route('/reverse_alarm', methods=['POST'])
+def reverse_alarm():
+    _str = request.from['alarm']
+    arr = _str.split(':')
+    _h = arr[0]
+    _min = arr[1]
+    tmp_json = reverse_alarm.calculateWaketimes(_h, _min)
+
+    return jsonify(tmp_json)
+    
+
 @app.route('/req', methods = ['POST'])
 def in_test():
     #string _origin, string _destination, string _move_sec, string _preparations, string _wake_up_time
@@ -20,11 +31,12 @@ def in_test():
     _destination = request.form['_destination']
     _mode = request.form['_mode']
     _move_sec = request.form['_move_sec']
+
     # _preparations = request.form['_preparations']
     # _wake_up_time = request.form['_wake_up_time']
 
-    tmp_json = map_api.call_map(_origin, _destination, _mode)
-    extra = extractor.extra_time(tmp_json, _move_sec, _preparations, _wake_up_time)
+    #tmp_json = map_api.call_map(_origin, _destination, _mode)
+    #extra = extractor.extra_time(tmp_json, _move_sec, _preparations, _wake_up_time)
     if extra == 0:
         return jsonify({'msg' : '0'})
     else:
