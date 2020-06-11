7a49a3536ffd3cf1a82f6537bfd66b2fcd1e505e
서민주 <minju1307@gmail.com>
2020-06-11 22:15:59 +0900

    Modify Alarm off clock

---
+++ src/Smalarm/app/src/main/java/com/example/smalarm/ui/alarm/CustomDigitalClock.java
@@ -0,0 +1,124 @@
+package com.example.smalarm.ui.alarm;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.database.ContentObserver;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.provider.Settings;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.format.DateFormat;
+import android.text.style.AbsoluteSizeSpan;
+import android.text.style.RelativeSizeSpan;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+/**
+ * You have to make a clone of the file CustomDigitalClock.java to use in your application, modify in the following manner:-
+ *      private final static String m12 = "h:mm aa";
+ *      private final static String m24 = "k:mm";
+ */
+
+public class CustomDigitalClock extends androidx.appcompat.widget.AppCompatTextView {
+
+    Calendar mCalendar;
+//    private final static String am_pm = "aa";
+    private final static String m12 = "aa hh:mm";
+    private final static String m24 = "k:mm";
+    private FormatChangeObserver mFormatChangeObserver;
+
+    private Runnable mTicker;
+    private Handler mHandler;
+
+    private boolean mTickerStopped = false;
+
+    String mFormat;
+
+    public CustomDigitalClock(Context context) {
+        super(context);
+        initClock(context);
+    }
+
+    public CustomDigitalClock(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initClock(context);
+    }
+
+    private void initClock(Context context) {
+        Resources r = context.getResources();
+
+        if (mCalendar == null) {
+            mCalendar = Calendar.getInstance();
+        }
+
+        mFormatChangeObserver = new FormatChangeObserver();
+        getContext().getContentResolver().registerContentObserver(
+                Settings.System.CONTENT_URI, true, mFormatChangeObserver);
+
+        setFormat();
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        mTickerStopped = false;
+        super.onAttachedToWindow();
+        mHandler = new Handler();
+
+        /**
+         * requests a tick on the next hard-second boundary
+         */
+        mTicker = new Runnable() {
+            public void run() {
+                if (mTickerStopped) return;
+                mCalendar.setTimeInMillis(System.currentTimeMillis());
+
+
+                setText(DateFormat.format(mFormat, mCalendar));
+
+                invalidate();
+                long now = SystemClock.uptimeMillis();
+                long next = now + (1000 - now % 1000);
+                mHandler.postAtTime(mTicker, next);
+            }
+        };
+        mTicker.run();
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        mTickerStopped = true;
+    }
+
+//    protected String getAm_pm() { return am_pm; }
+    /**
+     * Pulls 12/24 mode from system settings
+     */
+    private boolean get24HourMode() {
+        return android.text.format.DateFormat.is24HourFormat(getContext());
+    }
+
+    private void setFormat() {
+        if (get24HourMode()) {
+            mFormat = m24;
+        } else {
+            mFormat = m12;
+        }
+    }
+
+    private class FormatChangeObserver extends ContentObserver {
+        public FormatChangeObserver() {
+            super(new Handler());
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            setFormat();
+        }
+    }
+
+}
